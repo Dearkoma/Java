c8fae6e6185badfa1409e1e2734d11a09423b23a
Dearkoma <2981085607@qq.com>
2023-11-30 12:30:54 +0800

    test

--- hello/src/Map_/Properties_.java
+++ hello/src/Map_/Properties_.java
@@ -1,7 +1,15 @@
 package Map_;
 
+import java.util.Properties;
+
 public class Properties_{
     public static void main(String[] args) {
-        System.out.println("ddsd");
+        Properties pro =new Properties();
+        pro.put("1",1);
+        pro.put("2",2);
+        pro.put("3",3);
+        System.out.println(pro);
+        pro.put("1",11);
+        System.out.println(pro);
     }
 }
